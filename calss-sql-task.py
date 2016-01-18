7765e2b71a45d90e2549f21548cf467e4d037f93
EpeTukone <fren.ilya@tut.by>
2016-01-19 00:26:56 +0300

    test

--- calss-sql-task.py
+++ calss-sql-task.py
@@ -19,29 +19,28 @@ class Player(object):
         self.mute_status = 0
         self.ban_status = 0
 
-    def as_dict(self):
-
-        return dict_
 
     def load_to_db(self,):
         dict_ = {
         'email': str(self.email),
         'password': str(self.password),
         'name': str(self.name),
-        'type_account': int(self.type_account),
-        'mute_status': int(self.mute_status),
-        'ban_status': int(self.ban_status)
+        'type_account': self.type_account,
+        'mute_status': self.mute_status,
+        'ban_status': self.ban_status
         }
         # sql_query = """UPDATE player SET email=%(email); UPDATE player SET name=%(name)s;
         #  UPDATE player SET password=%(password); UPDATE player SET type_account=%(type_account);
         #  UPDATE player mute_status=%(mute_status); UPDATE player ban_status=$(ban_status)"""
         sql_query = """INSERT INTO `player` (`email`, `password`, `name`, `type_account`, `mute_status`, `ban_status`)
-            VALUES (email=%(email)s, password=%(password)s,
+            VALUES (email=%(email)s, password=%(password)s, name=%(name)s,
             type_account=%(type_account)s, mute_status=%(mute_status)s, ban_status=%(ban_status)s);
         """
         with con:
             cur = con.cursor()
             cur.execute(sql_query, dict_)
+        result = "load to db successful"
+        return result
 
     def __str__(self):
         format_str = 'email= "{}", password= "{}", name= "{}",\n' \
@@ -50,10 +49,14 @@ class Player(object):
                                  self.type_account, self.mute_status, self.ban_status )
 
 if __name__ == "__main__":
-    con = MySQLdb.connect('localhost', 'root', 'root', 'plyers');
+    con = MySQLdb.connect('localhost', 'root', 'mysql', 'players')
 ####### create a new player ######
     player = Player('mail.@tut.by', '12WW34e', 'drednout')
     print 'Create new player:\n', player, '\n',\
     player.load_to_db()
     '-----------------------------------------------------------------------'
 
+
+
+#delete FROM players.player
+#where id_player = 3
